ca23cff4cfa9e4a2969c04b02065c57807312f5a
CAravena797 <cristianaravena797@gmail.com>
2022-05-16 21:18:05 -0400

    Listo

---
+++ Evaluacion2ProgAvanzada/Evaluacion2/Comunicacion/HebraCliente.cs
@@ -0,0 +1,111 @@
+﻿using Ev2Model;
+using Ev2Model.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea1Biblioteca;
+
+namespace Evaluacion2.Comunicacion
+{
+    class HebraCliente
+    {
+        private ILecturasDAL lecturasDAL = LecturasDAL.GetInstancia();
+        private IMedidoresDAL medidoresDAL = MedidoresDAL.GetInstancia();
+        private ClienteCom clienteCom;
+
+        public HebraCliente(ClienteCom clienteCom)
+        {
+            this.clienteCom = clienteCom;
+        }
+
+        public void Ejecutar()
+        {
+            /*clienteCom.Escribir("Ingrese medidor: ");
+            string medidorId = clienteCom.Leer();
+            clienteCom.Escribir("Ingrese fecha (AAAA-MM-DD hh:mm:ss): ");
+            string fecha = clienteCom.Leer();
+            clienteCom.Escribir("Ingrese lectura: ");
+            string valor = clienteCom.Leer();
+            if (medidorId != null && fecha != null && valor != null)
+            {
+                List<Medidor> medidoresValidos = medidoresDAL.ObtenerMedidores();
+                string respuesta="";
+                foreach (Medidor medidor in medidoresValidos)
+                {
+                    if (medidor.Id == Convert.ToInt32(medidorId))
+                    {
+                        try
+                        {
+                            Lectura lectura = new Lectura()
+                            {
+                                MedidorId = Convert.ToInt32(medidorId),
+                                Fecha = DateTime.Parse(fecha),
+                                Valor = Convert.ToDouble(valor),
+                            };
+                            lock (lecturasDAL)
+                            {
+                                lecturasDAL.IngresarLectura(lectura);
+                            }
+                            respuesta = "OK";
+                            break;
+                            
+                        }catch (Exception ex)
+                        {
+                            clienteCom.Escribir("Error en formato");
+                            respuesta = "Medidor no valido";
+                            break;
+                        }
+                    } else
+                    {
+                        respuesta ="Medidor no valido";
+                    }
+                }
+                clienteCom.Escribir(respuesta);
+            }            
+            clienteCom.Desconectar();*/
+
+            string respuesta="";
+            try
+            {
+                string lectura = clienteCom.Leer();
+                string[] arr = lectura.Trim().Split('|');
+                if (arr[0] != null && arr[1] != null && arr[2] != null)
+                {
+                    List<Medidor> medidoresValidos = medidoresDAL.ObtenerMedidores();
+                    foreach (Medidor medidor in medidoresValidos)
+                    {
+                        if (medidor.Id == Convert.ToInt32(arr[0]))
+                        {
+                            try
+                            {
+                                lock (lecturasDAL)
+                                {
+                                    lecturasDAL.IngresarLectura(lectura);
+                                }
+                                respuesta = "OK";
+                                break;
+                            }
+                            catch (Exception ex)
+                            {
+                                respuesta = "Error en formato";
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            respuesta = "medidor no valido";
+                        }
+                    }
+                    clienteCom.Escribir(respuesta);
+                }
+            }catch (Exception ex)
+            {
+                clienteCom.Escribir("Error en formato");
+            }
+            
+            clienteCom.Desconectar();
+        }
+    }
+}
---
+++ Evaluacion2ProgAvanzada/Evaluacion2Cliente/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea1Biblioteca;
+
+namespace Evaluacion2Cliente
+{
+    internal class Program
+    {
+        /*static void Chat(ClienteSocket clienteSocket)
+        {
+            string mensaje = "";
+            string respuesta = "";
+
+            while (true)
+            {
+                mensaje = clienteSocket.Leer();
+                Console.WriteLine("server: {0}", mensaje);
+                respuesta = Console.ReadLine().Trim();
+                clienteSocket.Escribir(respuesta);
+                if (mensaje == "OK" | mensaje == "Medidor no valido" | mensaje == "Error en formato")
+                {
+                    clienteSocket.Desconectar();
+                    break;
+                }
+            }
+        }*/
+
+        static void Enviar(ClienteSocket clienteSocket)
+        {
+            Console.WriteLine("Ingrese medidor: ");
+            string medidor = Console.ReadLine().Trim();
+            Console.WriteLine("Ingrese fecha (AAAA-MM-DD hh:mm:ss)");
+            string fecha = Console.ReadLine().Trim();
+            fecha = fecha.Replace(" ", "-");
+            fecha = fecha.Replace(":", "-");
+            Console.WriteLine("Ingrese lectura: ");
+            string valor = Console.ReadLine().Trim();
+
+            string lectura = medidor + "|" + fecha + "|" + valor;
+
+            clienteSocket.Escribir(lectura);
+            string respuesta = clienteSocket.Leer();
+            Console.WriteLine("Server: "+respuesta);
+            clienteSocket.Desconectar();
+
+        }
+        static void Main(string[] args)
+        {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            string servidor = "127.0.0.1";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Conectado a Servidor {0} en puerto {1}", servidor, puerto);
+            ClienteSocket clienteSocket = new ClienteSocket(servidor, puerto);
+
+            if (clienteSocket.Conectar())
+            {
+                Console.WriteLine("Conectado...");
+                Enviar(clienteSocket);
+                //Chat(clienteSocket);
+            }
+            else
+            {
+                Console.WriteLine("Error de Comunicacion");
+            }
+        }
+    }
+}
